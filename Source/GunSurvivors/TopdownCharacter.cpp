2cf6861e5825e508d6fdaa79c424e303b3be9471
jmaqdah <jadmaqdah@gmail.com>
2024-09-12 22:37:21 +0100

    Adding all the files to the remote repo

---
+++ Source/GunSurvivors/TopdownCharacter.cpp
@@ -0,0 +1,33 @@
+
+
+#include "TopdownCharacter.h"
+
+ATopdownCharacter::ATopdownCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+    
+    CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComp"));
+    SetRootComponent(CapsuleComp);
+    
+    CharacterFlipbook = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("CharacterFlipbook"));
+    CharacterFlipbook->SetupAttachment(CapsuleComp);
+}
+
+void ATopdownCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATopdownCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ATopdownCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
